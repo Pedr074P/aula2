1ee2bbb1da54ff35a3926b1afa4f635adca4f5ff
Pedr074P <160858874+Pedr074P@users.noreply.github.com>
2024-04-12 08:34:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//declarando um array vazio
+var numeros=[];
+
+//adicionando elementos ao array
+numeros.push(4);
+numeros.push(10);
+
+//acessando elementos do array
+console.log(numeros[0]);
+console.log(numeros[1]);
